96b35e9a17bd1cd0d60074fa7043eda7ce1644dd
ThuyTien <thuytien233626@gmail.com>
2026-02-02 23:12:30 +0700

    add page login and connect db

--- DNC.InternshipSystem.Web/Controllers/AccountController.cs
+++ DNC.InternshipSystem.Web/Controllers/AccountController.cs
@@ -1,13 +1,69 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DNC.InternshipSystem.Core.Entities;
+using DNC.InternshipSystem.Web.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DNC.InternshipSystem.Web.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Login()
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(SignInManager<AppUser> signInManager, UserManager<AppUser> userManager)
         {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        // GET: /Account/Login
+        [HttpGet]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
             return View();
         }
-        
+
+        // POST: /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel model, string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+
+            if (ModelState.IsValid)
+            {
+                var result = await _signInManager.PasswordSignInAsync(
+                    model.Username,
+                    model.Password,
+                    model.RememberMe,
+                    lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToLocal(returnUrl);
+                }
+
+                ModelState.AddModelError(string.Empty, "Tên đăng nhập hoặc mật khẩu không đúng.");
+            }
+
+            return View(model);
+        }
+
+        // POST: /Account/Logout
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+
+        private IActionResult RedirectToLocal(string? returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+                return Redirect(returnUrl);
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
--- DNC.InternshipSystem.Web/Controllers/HomeController.cs
+++ DNC.InternshipSystem.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using DNC.InternshipSystem.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DNC.InternshipSystem.Web.Controllers;
 
+[Authorize]
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
